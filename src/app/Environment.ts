b5b800c2ef00c1c91a5f471b46318c685358a06d
Muhammad Muneeb Zafar <muneebazafar999@gmail.com>
2024-11-10 10:12:23 -0700

    ngrok

--- src/app/Environment.ts
+++ src/app/Environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  BASEURL:'https://localhost:44382/api'
+  // BASEURL:'https://localhost:44382/api'
   // BASEURL:'http://localhost:2030/api'
-  // BASEURL:'https://b96a-111-88-88-245.ngrok-free.app/api'
+  BASEURL:'https://78fa-111-88-88-245.ngrok-free.app/api'
 }
