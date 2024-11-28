c5dcc2dd83ce45decf7ae7aacc241929992f7750
Muhammad Muneeb Zafar <muneebazafar999@gmail.com>
2024-11-27 23:41:42 -0700

    env change

--- src/app/Environment.ts
+++ src/app/Environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   // BASEURL:'https://localhost:44382/api'
-  // BASEURL:'http://localhost:2030/api'
-  BASEURL:'https://dbe6-111-88-88-177.ngrok-free.app/api'
+  BASEURL:'http://localhost:2030/api'
+  // BASEURL:'https://dbe6-111-88-88-177.ngrok-free.app/api'
 }
